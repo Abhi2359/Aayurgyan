4ce765193bae91831a399c9ac5af3a904c041717
Abhi2359 <abhishekchoudhary1018@gmail.com>
2022-11-14 16:51:42 +0530

    commit

--- components/footer.js
+++ components/footer.js
@@ -20,7 +20,7 @@ export default function Footer() {
                     <Link href={"https://en.wikipedia.org/wiki/Medicinal_plants"}><ImWikipedia color="#1a1a00" className="w-16"/></Link>
                     <Link href={"https://www.youtube.com/watch?v=Kt0Lgy17I78"}><ImYoutube color="#1a1a00" /></Link>
                       </div>
-        <p className="py-5 text-black text-xl font-extrabold">Copyright ©2022 All rights reserved | This blogging website is made to access knowledge about medicinal plants</p>
+        <p className="py-5 text-black text-xl font-extrabold">Copyright ©2022 All rights reserved | This blogging website is made to get access of knowledge about medicinal plants</p>
          <p className="text-black hover:text-red-800 text-center cursor-pointer">Terms & Condition</p>
    </div>
   </div>
